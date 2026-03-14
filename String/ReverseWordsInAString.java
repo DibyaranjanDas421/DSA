7a1c95d582df8a82990906a4d570691743b7a257
DibyaranjanDas421 <dibyaranjandas468@gmail.com>
2026-03-14 21:52:07 +0530

    solved some probmes in string

---
+++ String/ReverseWordsInAString.java
@@ -0,0 +1,28 @@
+package String;
+
+import java.util.Arrays;
+
+public class ReverseWordsInAString {
+    public static void main(String[] args) {
+        String s = "the sky is blue";
+
+        System.out.println(reverseWords(s));
+
+    }
+
+    public static String reverseWords(String s) {
+        String s1=s.trim();
+        String[] words = s1.split(" ");
+        int start = 0;
+        int end = words.length - 1;
+        while (start < end) {
+            String temp;
+            temp = words[start];
+            words[start] = words[end];
+            words[end] = temp;
+            start++;
+            end--;
+        }
+        return String.join(" ", words);
+    }
+}
